bbd284f6110f9fe3765b2438fcf6dd8139ce0194
Daniel Bonnauer <bonnauer@predic8.de>
2022-04-29 14:04:19 +0200

    removed imports that weren't needed at this point

--- src/main/java/com/predic8/scooter/BonusApplication.java
+++ src/main/java/com/predic8/scooter/BonusApplication.java
@@ -2,10 +2,6 @@ package com.predic8.scooter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BonusApplication {
